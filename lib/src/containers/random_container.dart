b126df376628142d2f5ac0159fb315bde44484cc
antoniofpereir <antoniofpereir@gmail.com>
2018-12-10 18:42:04 +0000

    Added more experiments.

---
+++ lib/src/containers/random_container.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+var randomContainer = Container(
+  child: Text(
+    'sadfadfadf',
+    style: TextStyle(
+      fontSize: 28.0,
+      fontWeight: FontWeight.w800,
+      fontFamily: "Consolas",
+    ),
+  ),
+  color: Colors.black12,
+);
